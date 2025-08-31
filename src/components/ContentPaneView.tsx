edbc7f04142622e7da3b4a3f365c05d1f565ba4d
kangtae49 <kangtae49@gmail.com>
2025-09-01 08:58:43 +0900

    init screen

--- src/components/ContentPaneView.tsx
+++ src/components/ContentPaneView.tsx
@@ -1,13 +1,22 @@
-import ContentHeadView from "@/components/ContentHeadView.tsx";
 import ContentView from "@/components/ContentView.tsx";
 import {SplitPane} from "@rexxars/react-split-pane";
 import {useState} from "react";
 import TabView from "@/components/tab/TabView.tsx";
 import {useIsResizingStore} from "@/stores/isResizingStore.ts";
+import {useSelectedTreeItemStore} from "@/components/tree/stores/selectedTreeItemStore.ts";
 
 function ContentPaneView() {
   const {setIsResizing} = useIsResizingStore()
-
+  const {selectedItem} = useSelectedTreeItemStore();
+  if (selectedItem == undefined) {
+    return (
+      <div className="content-logo">
+        <div className="logo">
+          <img src="/tr-note.svg" alt="tr-note"/>
+        </div>
+      </div>
+    )
+  }
   return (
     <div className="content-pane">
       <SplitPane
